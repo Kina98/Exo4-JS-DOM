9cbb1d66ad4631ce07ca311402f8ebe397ea1392
Boubacar Kina <boubacarkina1@gmail.com>
2023-08-22 21:50:11 +0100

    JS DOM

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ auteurElement.textContent = chanson.auteur + " -";
 const titreChansonElement = document.createTextNode(chanson.titre);
 titreElement.appendChild(auteurElement);
 titreElement.appendChild(titreChansonElement);
-console.log(document.body.appendChild(titreElement));
+document.body.appendChild(titreElement);
 
 
 // 2. Créer une div avec la classe paroles
@@ -25,14 +25,29 @@ const parolesDiv = document.createElement('div');
 parolesDiv.className = "paroles";
 document.body.appendChild(parolesDiv);
 
-chanson.paroles.forEach(couplet => {
+chanson.paroles.forEach(element => {
     const paragraph = document.createElement('p');
-    const lignes = couplet.contenu.map(ligne => {
-        return ligne + "<br/>";
+    paragraph.classList = 'refrain';
+    const span = document.createElement('span');
+    span.classList = 'contenu'
+    const hidden = document.createElement('span');
+    
+        if (element.type === 'choeur') {
+            hidden.classList = 'choeur hidden';
+            hidden.innerHTML = "[Refrains]<br>";
+        } else {
+            hidden.classList = element.type;
+        }
+    
+    const lignes = element.contenu.map(ligne => {
+        return ligne + "<br>";
     }).join("");
-    paragraph.innerHTML = lignes;
     parolesDiv.appendChild(paragraph);
-})
+    paragraph.append(span);
+    paragraph.prepend(hidden);
+    span.innerHTML = lignes;
+
+});
 
 
 // 3. Ajout de footer
@@ -41,3 +56,84 @@ document.body.appendChild(footer);
 footer.textContent = "© Copyright 2023 - Codeloccol";
 
 // 4.
+const button1 = document.createElement('input');
+const button2 = document.createElement('input');
+
+const label1 = document.createElement('label');
+const label2 = document.createElement('label');
+const hrr = document.createElement('hr');
+document.body.insertBefore(hrr, parolesDiv);
+
+label1.innerText = "Masquer les Paroles";
+label2.innerText = "Masquer les refrains";
+
+label1.prepend(button1);
+label2.prepend(button2);
+
+button1.setAttribute('type', 'checkbox');
+button2.setAttribute('type', 'checkbox');
+button1.setAttribute('id', 'masquer-paroles');
+button2.setAttribute('id', 'masquer-refrains');
+button1.setAttribute('name', 'refrain');
+button2.setAttribute('name', 'refrain');
+
+document.body.prepend(label1);
+document.body.prepend(label2);
+
+
+
+// Récupere la case "masquer les paroles"
+const checkboxParoles = document.getElementById('masquer-paroles');
+const divParoles = document.getElementById('paroles');
+const labelParoles = checkboxParoles.nextSibling;
+const hr = document.querySelector('hr');
+
+button1.addEventListener('click', function() {
+  if (checkboxParoles.checked) {
+    parolesDiv.style.display = 'none';
+    labelParoles.textContent = 'Afficher les paroles';
+    hr.style.display = 'none';
+  } else {
+    parolesDiv.style.display = 'block';
+    labelParoles.textContent = 'Masquer les paroles';
+    hr.style.display = 'block';
+  }
+});
+
+
+// Remplacez tous les refrains sauf le premier par [Refrain]
+const checkboxRefrains = document.getElementById('masquer-refrains');
+const refrains = document.querySelectorAll('.refrain');
+const labelRefrains = checkboxRefrains.nextSibling;
+const definition = document.querySelectorAll('.choeur');
+const contenu = document.querySelectorAll('.contenu');
+
+button2.addEventListener('click', function(event) {
+  for (let i = 1; i < refrains.length; i++) {
+    if (event.target.checked) {
+      contenu[i].classList.add('hidden');
+      definition[i].classList.remove('hidden');
+      labelRefrains.textContent = 'Masquer les refrains';
+
+      definition[i].addEventListener('mouseenter', () => {
+          contenu[i].classList.remove('hidden');
+          });
+      definition[i].addEventListener('mouseleave', () => {
+        contenu[i].classList.add('hidden');
+        });
+    } else {
+      contenu[i].classList.remove('hidden');
+      definition[i].classList.add('hidden');
+      labelRefrains.textContent = 'Afficher les refrains';
+    }
+    
+  }
+})
+
+ const paragraph = document.querySelectorAll('p');
+ console.log(paragraph);
+ paragraph.forEach(element => {
+   element.setAttribute('style', 'margin: 0px');
+ });
+
+
